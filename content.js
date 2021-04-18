21890ec349d5afa0a65f6778f7dc5ac2ee007dec
ishanfdo18098 <e18098@eng.pdn.ac.lk>
2021-04-18 09:02:46 +0530

    changes the circle chart titles and percentage of off peak usage

--- content.js
+++ content.js
@@ -1,23 +1,43 @@
 console.log("SLT usage meter change data usage extension LOADED");
 
 setTimeout(() => {
+    //change the data usages
     const getEleClassNameArray = document.getElementsByClassName("sc-dnqmqq jZsdgY");
 
-    const standardUsage_text = getEleClassNameArray[0];
-    const freeUsage = code(standardUsage_text, 0);
+    //change Peak
+    const peakUsage_text = getEleClassNameArray[0];
+    const peakUsageArray = code(peakUsage_text, 0);
 
+    //change off peak
     const totalUsage_text = getEleClassNameArray[1];
-    code(totalUsage_text, freeUsage);
+    code(totalUsage_text, peakUsageArray[2], 1, peakUsageArray[1]);
+
+    //change the titles
+    const standardText = document.getElementsByClassName("sc-dnqmqq hZAKPK");
+    standardText[0].innerHTML = "Peak Data Usage";
+    standardText[1].innerHTML = "Off Peak Data Usage";
+
+
 }, 3000);
 
 
 
-function code(standardUsage_text, subtractThisPartToo) {
-    const textArray = standardUsage_text.textContent.split(" ");
+function code(Usage_text, subtractThisPartToo, isOffPeak = 0, totalPeak = 0) {
+    const textArray = Usage_text.textContent.split(" ");
     console.log(textArray);
     const used = textArray[0].slice(0, -2);
     const total = textArray[3].slice(0, -2);
     const dataUsageLeft = (parseFloat(total) - parseFloat(used) - subtractThisPartToo).toFixed(2);
-    standardUsage_text.innerHTML = dataUsageLeft.toString() + "GB Free of " + total.toString() + "GB (edited)";
-    return dataUsageLeft
+    Usage_text.innerHTML = dataUsageLeft.toString() + "GB Free of " + total.toString() + "GB (edited)";
+    //if its offpeak values
+    if (isOffPeak) {
+        Usage_text.innerHTML = dataUsageLeft.toString() + "GB Free of " + (total - totalPeak).toFixed(2) + "GB (edited)";
+
+        //change the graph percentage
+        document.getElementsByTagName("text")[1].innerHTML = (dataUsageLeft / (total - totalPeak)).toFixed(2) * 100 + "%";
+
+        //TODO: change the circle graph
+    }
+
+    return [used, total, dataUsageLeft]
 }
