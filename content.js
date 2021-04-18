6698e2d25206a5d30d8cc8ac4bb068a505d75310
ishanfdo18098 <e18098@eng.pdn.ac.lk>
2021-04-18 09:19:53 +0530

    changes the circle chart of off peak data usage

--- content.js
+++ content.js
@@ -25,18 +25,27 @@ setTimeout(() => {
 function code(Usage_text, subtractThisPartToo, isOffPeak = 0, totalPeak = 0) {
     const textArray = Usage_text.textContent.split(" ");
     console.log(textArray);
+
     const used = textArray[0].slice(0, -2);
     const total = textArray[3].slice(0, -2);
     const dataUsageLeft = (parseFloat(total) - parseFloat(used) - subtractThisPartToo).toFixed(2);
+
     Usage_text.innerHTML = dataUsageLeft.toString() + "GB Free of " + total.toString() + "GB (edited)";
+
     //if its offpeak values
     if (isOffPeak) {
+        //change the data left
         Usage_text.innerHTML = dataUsageLeft.toString() + "GB Free of " + (total - totalPeak).toFixed(2) + "GB (edited)";
 
         //change the graph percentage
-        document.getElementsByTagName("text")[1].innerHTML = (dataUsageLeft / (total - totalPeak)).toFixed(2) * 100 + "%";
+        const offPeakUsedPercentage = (dataUsageLeft / (total - totalPeak)).toFixed(2) * 100
+        document.getElementsByTagName("text")[1].innerHTML = offPeakUsedPercentage + "%";
 
-        //TODO: change the circle graph
+        //change the circle graph
+        const offPeakCircleGraph = document.getElementsByTagName("circle")[3];
+        newCalculateValue = 1100 - 1100 * offPeakUsedPercentage / 100;
+        console.log("new circle graph strok-dashoffset value " + newCalculateValue);
+        offPeakCircleGraph.style = "stroke-dashoffset: " + newCalculateValue;
     }
 
     return [used, total, dataUsageLeft]
